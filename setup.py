704f6177bb3ca7fc8a244e88c6151a5fb6f2c05a
Am0rphous <29645801+Am0rphous@users.noreply.github.com>
2021-02-16 00:14:59 +0100

    small path change

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ from setuptools import setup
 APP = ['radiobar.py']
 APP_NAME = "RadioBar"
 APP_VERSION = "0.1"
-DATA_FILES = ['channels.json','remote.py','radio-icon.png','radio-icon-grey.png','radio-icon-green.png']
+DATA_FILES = ['channels.json','remote.py','icons/radio-icon.png','icons/radio-icon-grey.png','icons/radio-icon-green.png']
 OPTIONS = {
     'argv_emulation': False,
-    'iconfile': 'radiobar.icns',
+    'iconfile': 'icons/radiobar.icns',
     'plist': {
         'CFBundleName': APP_NAME,
         'CFBundleDisplayName': APP_NAME,
-        'CFBundleGetInfoString': "Listen to radio in your menubar",
+        'CFBundleGetInfoString': "Listen to radio in your macOS menubar",
         'CFBundleIdentifier': "com.mdbraber.radiobar",
         'CFBundleVersion': APP_VERSION,
         'CFBundleShortVersionString': APP_VERSION,
